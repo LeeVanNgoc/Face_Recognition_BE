f1c92ecb180aa4d665f6e085ac50e23a5eede5f9
Lee Ngoc <lee.ngoc6462@gmail.com>
2025-04-24 20:32:31 +0700

    Update db

--- Attendance/model.py
+++ Attendance/model.py
@@ -1,12 +1,15 @@
+from datetime import datetime
 from config.config import db
 
 class Attendance(db.Model):
     __tablename__ = 'attendance'  # Tên bảng trong DB (tùy chọn, nếu không thì mặc định là tên lớp)
 
     id = db.Column(db.Integer, primary_key=True)  # Khóa chính
-    userId = db.Column(db.String(100))  # userId, kiểu dữ liệu là String
-    timeCheck = db.Column(db.Date)  # Thay String bằng Date nếu lưu ngày tháng
-    typeCheck = db.Column(db.Integer)  # typeCheck không cần unique, chỉ cần lưu trạng thái
+    createdAt = db.Column(db.DateTime, default=datetime.utcnow)
+    updatedAt = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    userId = db.Column(db.String(36))  # userId, kiểu dữ liệu là String
+    timeAtten = db.Column(db.Date)  # Thay String bằng Date nếu lưu ngày tháng
+    image = db.Column(db.String(5000))  # typeCheck không cần unique, chỉ cần lưu trạng thái
 
     @classmethod
     def get_all_attendances(cls):
--- User/route.py
+++ User/route.py
@@ -1,11 +1,13 @@
 from flask import Blueprint
-from User.controller import create_user, get_all_users, get_user_by_id, update_user, delete_user
+from User.controller import create_user, get_all_users, get_user_by_id, update_user, delete_user, sign_in_user, log_in_user
 
 user_bp = Blueprint('user', __name__)
 
 # Đăng ký các route cho API
 user_bp.route('/users', methods=['POST'])(create_user)           # Tạo người dùng
 user_bp.route('/users', methods=['GET'])(get_all_users)         # Lấy tất cả người dùng
+user_bp.route('/auth/signup', methods=['POST'])(sign_in_user)   # Đăng ký tài khoản
+user_bp.route('/auth/login', methods=['POST'])(log_in_user)     # Đăng nhập
 user_bp.route('/users/<int:user_id>', methods=['GET'])(get_user_by_id)  # Lấy người dùng theo ID
 user_bp.route('/users/<int:user_id>', methods=['PUT'])(update_user)     # Cập nhật người dùng
 user_bp.route('/users/<int:user_id>', methods=['DELETE'])(delete_user)  # Xóa người dùng
